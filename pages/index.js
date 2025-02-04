bd0ad36b9bf5721ee65daecdd4aee3cf70edacb6
Éder de Rosso <82277906+EderRosso@users.noreply.github.com>
2025-02-04 01:24:11 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home(){
-    return <h1>Sofia, te amo. Se você me ama, faz um coração com as mãos!!!</h1>
+function Home() {
+  return (
+    <h1>Sofia, te amo. Se você me ama, faz um coração com as mãos!!!</h1>
+  );
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indenta��o errada");
+}
 export default Home;
